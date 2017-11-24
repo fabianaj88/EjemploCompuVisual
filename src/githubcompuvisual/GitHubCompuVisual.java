40dc3bbc3954e42ca5628160e67d3b56cc22f98e
Yoaner <Yoaner@Fabiana>
2017-11-23 19:22:42 -0500

    Hola Richar yo escribi un mensaje

--- src/githubcompuvisual/GitHubCompuVisual.java
+++ src/githubcompuvisual/GitHubCompuVisual.java
@@ -5,6 +5,8 @@
  */
 package githubcompuvisual;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Yoaner
@@ -16,6 +18,7 @@ public class GitHubCompuVisual {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Hola Richard");
     }
     
 }
